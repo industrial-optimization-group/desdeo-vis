512d22cab807d732851b0c67be3af052cc48426a
Frankie Robertson <frankie@robertson.name>
2018-07-20 17:51:52 +0300

    Move notebooks to package `desdeo_notebooks`

--- desdeo_notebooks/__main__.py
+++ desdeo_notebooks/__main__.py
@@ -3,8 +3,7 @@ from os import mkdir
 from os.path import isdir, join as pjoin
 from shutil import copyfileobj
 
-from pkg_resources import (
-    Requirement, resource_stream, resource_listdir, resource_isdir)
+from pkg_resources import resource_stream, resource_listdir, resource_isdir
 
 if len(sys.argv) >= 2:
     dest_dir = sys.argv[1]
@@ -14,14 +13,14 @@ else:
 if not isdir(dest_dir):
     mkdir(dest_dir)
 
-desdeo_vis = Requirement.parse('desdeo_vis')
+desdeo_notebooks = 'desdeo_notebooks'
 
-for nbf in resource_listdir(desdeo_vis, 'notebooks'):
+for nbf in resource_listdir(desdeo_notebooks, 'notebooks'):
     if not nbf.endswith('.ipynb'):
         continue
     path = 'notebooks/' + nbf
-    if resource_isdir(desdeo_vis, path):
+    if resource_isdir(desdeo_notebooks, path):
         continue
     copyfileobj(
-        resource_stream(desdeo_vis, path),
+        resource_stream(desdeo_notebooks, path),
         open(pjoin(dest_dir, nbf), 'wb'))
