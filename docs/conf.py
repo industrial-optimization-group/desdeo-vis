a5de5f5fcc37c8be5f1cbd2524441b42c7a3849e
Frankie Robertson <frankie@robertson.name>
2018-08-27 15:57:25 +0300

    Fix binder notebook links

--- docs/conf.py
+++ docs/conf.py
@@ -187,7 +187,7 @@ nbsphinx_prolog = r"""
         Run this example in your browser
 
         | This page was generated from `{{ docname }}`__.
-        | Interactive online version: :raw-html:`<a href="https://mybinder.org/v2/gh/industrial-optimization-group/desdeo-vis/{{ env.config.release }}?filepath=notebooks%2F{{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge.svg" style="vertical-align:text-bottom"></a>` (without output) :raw-html:`<a href="https://mybinder.org/v2/gh/industrial-optimization-group/desdeo-vis/{{ env.config.release }}?filepath=output-notebooks%2F{{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge.svg" style="vertical-align:text-bottom"></a>` (with output)
+        | Interactive online version: :raw-html:`<a href="https://mybinder.org/v2/gh/industrial-optimization-group/desdeo-vis/{{ env.config.release }}?filepath=desdeo_notebooks%2F{{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge.svg" style="vertical-align:text-bottom"></a>` (without output) :raw-html:`<a href="https://mybinder.org/v2/gh/industrial-optimization-group/desdeo-vis/{{ env.config.release }}?filepath=desdeo_notebooks%2Foutput%2F{{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge.svg" style="vertical-align:text-bottom"></a>` (with output)
 
     __ https://github.com/industrial-optimization-group/desdeo-vis/blob/
         {{ env.config.release }}/output-notebooks/{{ docname }}
