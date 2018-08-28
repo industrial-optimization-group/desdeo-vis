7b1b7611f2d3a19d4d8ed18a28785fc79bfbbcb1
Frankie Robertson <frankie@robertson.name>
2018-08-28 10:55:23 +0300

    Add docstrings to conf module

--- desdeo_vis/conf.py
+++ desdeo_vis/conf.py
@@ -1,12 +1,32 @@
+"""
+Globally configure default settings for plotting and widgets.
+"""
+
 _settings = {
     'max_as_min': True
 }
 
 
 def conf(**kwargs):
+    """
+    Set configuration values
+
+    Parameters
+    ----------
+    max_as_min
+        Whether to reformulate maximized functions as minimized functions.
+    """
     for k, v in kwargs.items():
         _settings[k] = v
 
 
-def get_conf(setting):
+def get_conf(setting: str):
+    """
+    Get a configuration value
+
+    Parameters
+    ----------
+    setting
+        The configuration key to get.
+    """
     return _settings[setting]
