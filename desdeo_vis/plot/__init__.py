96c04b72baf6ba503c8978c1e92783733271ac97
Frankie Robertson <frankie@robertson.name>
2018-08-28 10:55:08 +0300

    Add docstrings to main top level modules

--- desdeo_vis/plot/__init__.py
+++ desdeo_vis/plot/__init__.py
@@ -1,3 +1,8 @@
+"""
+This module contains the machinery for doing non-interactive plotting of
+multidimensional data. Currently it can generate VEGA specifications for
+parallel coordinates plots.
+"""
 __all__ = ['parplot']
 
 from .parallel import vega3_parplot as parplot
--- desdeo_vis/widget/__init__.py
+++ desdeo_vis/widget/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""
+This module contains Jupyter widgets for interactively specifying preferences
+as well as interactively and non-interactively displaying solutions.
+"""
+
 from .widgets import VegaWidget, NimbusPrefWidget, ParplotWidget
 
 __all__ = ['VegaWidget', 'NimbusPrefWidget', 'ParplotWidget']
